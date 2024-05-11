1de6c7aa029028aada65080db4027e67ebfae8ce
sarbador <avaz.shams.2002@gmail.com>
2024-05-12 00:00:11 +0500

    making add_contact functionality

--- app/Http/Controllers/ContactsController.php
+++ app/Http/Controllers/ContactsController.php
@@ -3,6 +3,7 @@
 namespace App\Http\Controllers;
 
 use App\Models\User;
+use App\Models\UserContact;
 use App\Traits\ResponsesTrait;
 use Illuminate\Http\Request;
 
@@ -12,10 +13,14 @@ class ContactsController extends Controller
     use ResponsesTrait;
     public function search_contacts(Request $request)
     {
+
+        $current_user = $request->user();
+
         $users = User::where(function ($sql) use ($request) {
             $sql->where('email', 'like', "%{$request->get('value')}%")
                 ->orWhere('user_name', 'like', "%{$request->get("value")}%");
         })
+            ->where("id", "!=", $current_user->id)
             ->whereNull('deleted_at')
             ->paginate($request->get('per_page') ?? 15);
 
@@ -24,5 +29,24 @@ class ContactsController extends Controller
 
     public function add_contact(Request $request)
     {
+        $current_user = $request->user();
+
+        $check_whether_contact_exists = UserContact
+            ::where('user_id', $current_user->id)
+            ->where("contact_id", $request->get('contact_id'))
+            ->get();
+
+        if (count($check_whether_contact_exists) >= 1) {
+            return $this->fail(['message' => "User alredy has been added to contacts"]);
+        }
+
+
+        $contact = UserContact::create([
+            'user_id' => $check_whether_contact_exists->id,
+            'contact_id' => $request->get('contact_id'),
+            'created_at' => date("Y-m-d H:i:s")
+        ]);
+
+        return $this->success(['contact' => $contact]);
     }
 }
--- database/migrations/2024_05_11_184647_user_contacts.php
+++ database/migrations/2024_05_11_184647_user_contacts.php
@@ -13,7 +13,22 @@ return new class extends Migration
      */
     public function up()
     {
-        //
+        Schema::create('user_contacts', function (Blueprint $table) {
+            $table->id();
+            $table->unsignedBigInteger('user_id')->nullable();
+            $table->unsignedBigInteger('contact_id')->nullable();
+            $table->dateTime('created_at')->nullable();
+            $table->dateTime('updated_at')->nullable();
+
+
+            $table->foreign('user_id', "foreign_key_for_user_contacts_user_id")
+                ->on('users')->references('id')
+                ->onDelete('set null')->onUpdate('cascade');
+
+            $table->foreign('contact_id', "foreign_key_for_user_contacts_contact_id")
+                ->on('users')->references('id')
+                ->onDelete('set null')->onUpdate('cascade');
+        });
     }
 
     /**
@@ -23,6 +38,6 @@ return new class extends Migration
      */
     public function down()
     {
-        //
+        Schema::dropIfExists('user_contacts');
     }
 };
