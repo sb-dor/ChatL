5fae77de76304934f53753a03aa64ffdfd3bce87
sarbador <avaz.shams.2002@gmail.com>
2024-05-12 13:14:30 +0500

    done with adding and searching contacts

--- app/Http/Controllers/ContactsController.php
+++ app/Http/Controllers/ContactsController.php
@@ -5,6 +5,7 @@ namespace App\Http\Controllers;
 use App\Models\User;
 use App\Models\UserContact;
 use App\Traits\ResponsesTrait;
+use Exception;
 use Illuminate\Http\Request;
 
 class ContactsController extends Controller
@@ -16,10 +17,14 @@ class ContactsController extends Controller
 
         $current_user = $request->user();
 
-        $users = User::where(function ($sql) use ($request) {
-            $sql->where('email', 'like', "%{$request->get('value')}%")
-                ->orWhere('user_name', 'like', "%{$request->get("value")}%");
-        })
+        $users = User
+            ::with(['user_contact' => function ($sql) use ($current_user) {
+                $sql->where("user_id", $current_user->id);
+            }])
+            ->where(function ($sql) use ($request) {
+                $sql->where('email', 'like', "%{$request->get('value')}%")
+                    ->orWhere('user_name', 'like', "%{$request->get("value")}%");
+            })
             ->where("id", "!=", $current_user->id)
             ->whereNull('deleted_at')
             ->paginate($request->get('per_page') ?? 15);
@@ -29,24 +34,27 @@ class ContactsController extends Controller
 
     public function add_contact(Request $request)
     {
-        $current_user = $request->user();
+        try {
+            $current_user = $request->user();
 
-        $check_whether_contact_exists = UserContact
-            ::where('user_id', $current_user->id)
-            ->where("contact_id", $request->get('contact_id'))
-            ->get();
+            $check_whether_contact_exists = UserContact
+                ::where('user_id', $current_user->id)
+                ->where("contact_id", $request->get('contact_id'))
+                ->get();
 
-        if (count($check_whether_contact_exists) >= 1) {
-            return $this->fail(['message' => "User alredy has been added to contacts"]);
+            if (count($check_whether_contact_exists) >= 1) {
+                return $this->fail(['message' => "User alredy has been added to contacts"]);
+            }
+
+            $contact = UserContact::create([
+                'user_id' => $current_user->id,
+                'contact_id' => $request->get('contact_id'),
+                'created_at' => date("Y-m-d H:i:s")
+            ]);
+
+            return $this->success(['contact' => $contact]);
+        } catch (Exception $e) {
+            return $this->fail(['message' => $e->getMessage()]);
         }
-
-
-        $contact = UserContact::create([
-            'user_id' => $check_whether_contact_exists->id,
-            'contact_id' => $request->get('contact_id'),
-            'created_at' => date("Y-m-d H:i:s")
-        ]);
-
-        return $this->success(['contact' => $contact]);
     }
 }
--- app/Models/User.php
+++ app/Models/User.php
@@ -44,4 +44,10 @@ class User extends Authenticatable
     protected $casts = [
         'email_verified_at' => 'datetime',
     ];
+
+
+    public function user_contact()
+    {
+        return $this->hasOne(UserContact::class, 'contact_id', 'id');
+    }
 }
