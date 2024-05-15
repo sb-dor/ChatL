fcb484829a3d7ed18058437ffdf52fb57626f71a
sarbador <avaz.shams.2002@gmail.com>
2024-05-15 22:31:25 +0500

    making chat message ui

--- app/Http/Controllers/ChatController.php
+++ app/Http/Controllers/ChatController.php
@@ -21,8 +21,10 @@ class ChatController extends Controller
 
         $user = $request->user();
 
-        $chats = User::join('chat_participants', 'chats.id', 'chat_participants.chat_id')
+        $chats = User
+            ::join('chat_participants', 'chats.id', 'chat_participants.chat_id')
             ->where('chat_participants.id', $user->id)
+            ->whereNull('temporary_chat')
             ->select('chats.*')
             ->with('chat_last_message')
             ->get();
@@ -41,6 +43,8 @@ class ChatController extends Controller
             $findChat = Chat::where('chat_uuid', $request->get('chat_uuid'))->first();
 
             if (!$findChat) {
+
+                // $this->find_and_delete_users_temporary_created_chat($user_ids);
 
                 $time = date('Y-m-d H:i:s');
 
@@ -71,13 +75,54 @@ class ChatController extends Controller
 
     public function delete_temp_created_chats(Request $request)
     {
+        Chat::where('id', $request->get('chat_id'))
+            ->where('chat_uuid', $request->get('chat_uuid'))
+            ->whereNotNull('temporary_chat')
+            ->delete();
+
+        return $this->success(['chat_id' => $request->get('chat_id'), "chat_uuid" => $request->get('chat_uuid')]);
     }
+
+    private function find_and_delete_users_temporary_created_chat($user_ids = [])
+    {
+
+        $usersChatsIds = Chat::leftJoin('chat_participants', 'chats.id', 'chat_participants.chat_id')
+            ->where(function ($sql) use ($user_ids) {
+                for ($i = 0; $i < count($user_ids); $i++) {
+                    if ($i == 0) {
+                        $sql->where('chat_participants.user_id', $user_ids[$i]);
+                    } else {
+                        $sql->where('chat_participants.user_id', $user_ids[$i]);
+                    }
+                }
+            })
+            ->groupBy('chats.id')
+            ->select('chats.id')
+            ->pluck('chats.id')
+            ->toArray();
+
+        Chat::where('id', $usersChatsIds)->delete();
+    }
+
 
     public function test_message()
     {
-        $chat = new Chat();
-        $channel_name = "chat_notify_of_user_2";
+        // $chat = new Chat();
+        // $channel_name = "chat_notify_of_user_2";
 
-        event(new ChatNotifyEvent($channel_name, $chat));
+        // event(new ChatNotifyEvent($channel_name, $chat));
+
+        // $ids = [1, 2];
+
+        // $usersChat = Chat::leftJoin('chat_participants', 'chats.id', 'chat_participants.chat_id')
+        //     ->where(function ($sql) use ($ids) {
+
+        //     })
+        //     ->groupBy('chats.id')
+        //     ->select('chats.id')
+        //     ->pluck('chats.id')
+        //     ->toArray();
+
+        // return $usersChat;
     }
 }
