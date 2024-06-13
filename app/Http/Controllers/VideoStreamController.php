dd0e4402182b251a9811883de701fb14278d4af9
sb-dor <avaz.shams.2002@gmail.com>
2024-06-13 07:07:16 +0500

    wrote chat entrance

--- app/Http/Controllers/VideoStreamController.php
+++ app/Http/Controllers/VideoStreamController.php
@@ -2,6 +2,9 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\Chat;
+use App\Models\ChatParticipant;
+use Exception;
 use Illuminate\Http\Request;
 
 class VideoStreamController extends Controller
@@ -11,6 +14,33 @@ class VideoStreamController extends Controller
     {
         // check first maybe someone else started that chat but user 
         // didn't handle that
+        try {
+            $chat = Chat
+                ::where('id', $request->get("chat_id"))
+                ->where("chat_uuid", $request->get("chat_uuid"))
+                ->first();
+
+            if (!$chat) {
+                //
+            }
+
+            $chat->update([
+                'video_chat_streaming' => true,
+            ]);
+
+            $find_participant = ChatParticipant::firstOrCreate([
+                'chat_id' => $chat->id,
+                'user_id' => $request->get('user_id'),
+                'in_video_stream' => true,
+                'participate_at' => date('Y-m-d H:i:s'),
+            ]);
+
+            return response()->json(['success' => true]);
+
+        } catch (Exception $e) {
+            return response()->json(['message' => $e->getMessage()]);
+        }
+
     }
 
     // for specific new chat participant
